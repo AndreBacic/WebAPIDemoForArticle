68f2e5e0b8e1c1bad1a1e447ecd0b8c574fadb04
andre9050@yahoo.com <andre9050@yahoo.com>
2022-05-20 17:33:45 -0400

    Add project files.

---
+++ WebAPIDemoForArticle/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿async function renderForecastData() {
+    let apiResponse = await fetch('/weatherForecast')
+    let forecasts = await apiResponse.json()
+
+    forecasts.forEach(forecast => {
+        let p = document.createElement('p')
+        p.innerText = `${forecast.date} - ${forecast.temperatureF}°F/${forecast.temperatureC}°C - ${forecast.summary}`
+        document.getElementById('main').appendChild(p)
+    })
+}
+renderForecastData().then()
